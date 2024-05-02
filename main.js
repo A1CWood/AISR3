950dc767a71c84cdd8d10487ad90dc7445c45733
A1CWood <168307546+A1CWood@users.noreply.github.com>
2024-05-02 10:14:48 -0800

    Update main.js
    idk

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ function generate() {
     var iff = split[4];
     var output = [];
 
+    if(!typeof callcount === 'number')){
+         callcount= split[2].substr(split[2].length - 1)
+         precall = split[2].substr(0, split[2].length - 1);
+    }
     
     for (var i = 0; i < outputq - 1; i++) {
         tencount++;
